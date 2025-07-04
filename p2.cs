b86546a6a303cfbc9bb4af05336d1c00e82e3ddf
Dell <Dell@DESKTOP-NT99K2H>
2025-07-04 11:32:45 +0530

    Add project files.

---
+++ p2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo
+{
+    internal class p2
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple C# program that prints "Hello, World!" to the console.
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
